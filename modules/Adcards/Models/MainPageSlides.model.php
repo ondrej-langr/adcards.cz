dc20bdf751368c2ce637910d4249e211d9ee678d
Ondřej Langr <hi@ondrejlangr.cz>
2023-10-06 18:24:59 +0200

    Add mainpage slides model

---
+++ modules/Adcards/Models/MainPageSlides.model.php
@@ -0,0 +1,144 @@
+<?php
+
+use PromCMS\Core\Database\Model;
+use PromCMS\Core\Database\SingletonModel;
+use PromCMS\Core\Database\ModelResult;
+
+class MainPageSlides extends Model
+{
+  protected string $tableName = 'main_page_slides';
+  protected bool $softDelete = false;
+
+  protected bool $timestamps = true;
+  protected bool $translations = true;
+
+  public static array $tableColumns = [
+    'id' => [
+      'title' => 'ID',
+      'hide' => false,
+      'required' => false,
+      'unique' => true,
+      'editable' => false,
+      'translations' => false,
+      'admin' => ['isHidden' => false, 'editor' => ['placement' => 'main']],
+      'type' => 'number',
+      'autoIncrement' => true,
+    ],
+
+    'title' => [
+      'title' => 'Titulek',
+      'hide' => false,
+      'required' => true,
+      'unique' => true,
+      'editable' => true,
+      'translations' => true,
+      'admin' => ['isHidden' => false, 'editor' => ['placement' => 'main']],
+      'type' => 'longText',
+    ],
+
+    'subTitle' => [
+      'title' => 'Subtitulek',
+      'hide' => false,
+      'required' => false,
+      'unique' => false,
+      'editable' => true,
+      'translations' => true,
+      'admin' => [
+        'isHidden' => false,
+        'editor' => ['placement' => 'main'],
+        'fieldType' => 'normal',
+      ],
+      'type' => 'string',
+    ],
+
+    'buttonUrl' => [
+      'title' => 'Odkaz pro tlačítko',
+      'hide' => false,
+      'required' => false,
+      'unique' => false,
+      'editable' => true,
+      'translations' => true,
+      'admin' => [
+        'isHidden' => false,
+        'editor' => ['placement' => 'main'],
+        'fieldType' => 'normal',
+      ],
+      'type' => 'string',
+    ],
+
+    'image' => [
+      'title' => 'Obrázek',
+      'hide' => false,
+      'required' => true,
+      'unique' => false,
+      'editable' => true,
+      'translations' => false,
+      'admin' => [
+        'isHidden' => false,
+        'editor' => ['placement' => 'main'],
+        'fieldType' => 'normal',
+      ],
+      'type' => 'file',
+      'multiple' => false,
+      'typeFilter' => 'image',
+    ],
+
+    'created_by' => [
+      'title' => 'Created by',
+      'hide' => false,
+      'required' => false,
+      'unique' => false,
+      'editable' => false,
+      'translations' => false,
+      'admin' => ['isHidden' => true, 'editor' => ['placement' => 'main']],
+      'type' => 'relationship',
+      'targetModel' => 'user',
+      'labelConstructor' => 'name',
+      'multiple' => false,
+      'fill' => false,
+      'foreignKey' => 'id',
+    ],
+
+    'updated_by' => [
+      'title' => 'Updated by',
+      'hide' => false,
+      'required' => false,
+      'unique' => false,
+      'editable' => false,
+      'translations' => false,
+      'admin' => ['isHidden' => true, 'editor' => ['placement' => 'main']],
+      'type' => 'relationship',
+      'targetModel' => 'user',
+      'labelConstructor' => 'name',
+      'multiple' => false,
+      'fill' => false,
+      'foreignKey' => 'id',
+    ],
+  ];
+
+  static bool $ignoreSeeding = false;
+
+  static string $title = 'Slider Položky';
+
+  static string $modelIcon = 'Slideshow';
+  static $adminSettings = [];
+
+  public function getSummary()
+  {
+    return (object) [
+      'isSingleton' => $this instanceof SingletonModel,
+      'tableName' => $this->getTableName(),
+      'icon' => self::$modelIcon,
+      'title' => isset(self::$title) ? self::$title : null,
+      'ignoreSeeding' => self::$ignoreSeeding,
+      'admin' => self::$adminSettings,
+      'columns' => static::$tableColumns,
+      'hasTimestamps' => $this->hasTimestamps(),
+      'hasSoftDelete' => $this->hasSoftDelete(),
+      'ownable' => true,
+      'hasOrdering' => false,
+      'isDraftable' => false,
+      'isSharable' => false,
+    ];
+  }
+}
