aeea87cde36f07b51277f5f73973660d65374cc6
Ondřej Langr <hi@ondrejlangr.cz>
2023-10-28 10:05:51 +0200

    Cart and builder update

---
+++ modules/Adcards/Models/Sports.model.php
@@ -0,0 +1,109 @@
+<?php
+
+use PromCMS\Core\Database\Model;
+use PromCMS\Core\Database\SingletonModel;
+use PromCMS\Core\Database\ModelResult;
+
+class Sports extends Model
+{
+  protected string $tableName = 'sports';
+  protected bool $softDelete = false;
+
+  protected bool $timestamps = false;
+  protected bool $translations = true;
+  protected static bool $enabled = true;
+  protected static array $adminSettings = ['hidden' => false];
+
+  public static array $tableColumns = [
+    'id' => [
+      'title' => 'ID',
+      'hide' => false,
+      'required' => false,
+      'unique' => true,
+      'editable' => false,
+      'translations' => false,
+      'admin' => [
+        'isHidden' => false,
+        'editor' => ['placement' => 'main', 'width' => 12],
+      ],
+      'readonly' => false,
+      'type' => 'number',
+      'autoIncrement' => true,
+    ],
+
+    'name' => [
+      'title' => 'Název',
+      'hide' => false,
+      'required' => true,
+      'unique' => true,
+      'editable' => true,
+      'translations' => true,
+      'admin' => [
+        'isHidden' => false,
+        'editor' => ['placement' => 'main', 'width' => 12],
+        'fieldType' => 'heading',
+      ],
+      'readonly' => false,
+      'type' => 'string',
+    ],
+
+    'description' => [
+      'title' => 'Popisek',
+      'hide' => false,
+      'required' => false,
+      'unique' => false,
+      'editable' => true,
+      'translations' => true,
+      'admin' => [
+        'isHidden' => false,
+        'editor' => ['placement' => 'main', 'width' => 12],
+      ],
+      'readonly' => false,
+      'type' => 'longText',
+    ],
+
+    'image' => [
+      'title' => 'Obrázek',
+      'hide' => false,
+      'required' => true,
+      'unique' => false,
+      'editable' => true,
+      'translations' => false,
+      'admin' => [
+        'isHidden' => false,
+        'editor' => ['placement' => 'main', 'width' => 12],
+        'fieldType' => 'big-image',
+      ],
+      'readonly' => false,
+      'type' => 'file',
+      'multiple' => false,
+      'typeFilter' => 'image',
+    ],
+  ];
+
+  static bool $ignoreSeeding = false;
+
+  static string $title = 'Sporty';
+
+  static string $modelIcon = 'BallFootball';
+
+  public function getSummary()
+  {
+    return (object) [
+      'isSingleton' => $this instanceof SingletonModel,
+      'tableName' => $this->getTableName(),
+      'icon' => self::$modelIcon,
+      'title' => isset(self::$title) ? self::$title : null,
+      'ignoreSeeding' => self::$ignoreSeeding,
+      'columns' => static::$tableColumns,
+      'hasTimestamps' => $this->hasTimestamps(),
+      'hasSoftDelete' => $this->hasSoftDelete(),
+      'admin' => self::$adminSettings,
+      'enabled' => self::$enabled,
+      'ownable' => false,
+      'hasOrdering' => false,
+      'isDraftable' => false,
+      'isSharable' => false,
+    ];
+  }
+}
