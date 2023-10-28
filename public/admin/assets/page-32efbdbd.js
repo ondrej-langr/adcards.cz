3a9ea0d091c7357e01b70587af33b3e8ee6c60f9
Ondřej Langr <hi@ondrejlangr.cz>
2023-10-28 10:10:11 +0200

    Update prom admin

--- public/admin/assets/page-32efbdbd.js
+++
@@ -1,1 +0,0 @@
-import{j as e,z as r}from"./index-c439a344.js";import{C as t,a}from"./Content-093bf3cf.js";const n=()=>e.jsx(r,{children:e.jsx(t,{view:"create",children:e.jsx(a,{})})});export{n as default};
