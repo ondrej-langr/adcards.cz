88e38a27a3fc653557ea07b22ff6013cf68fb451
Ondřej Langr <hi@ondrejlangr.cz>
2023-10-30 11:45:15 +0100

    Update admin

---
+++ public/admin/assets/page-0b3d1c78.js
@@ -0,0 +1,1 @@
+import{j as e,z as r}from"./index-48692a22.js";import{C as t,a}from"./Content-7d002b29.js";const n=()=>e.jsx(r,{children:e.jsx(t,{view:"create",children:e.jsx(a,{})})});export{n as default};
---
+++ public/admin/assets/page-3875002f.js
@@ -0,0 +1,1 @@
+import{j as e,z as r}from"./index-48692a22.js";import{C as t,a as s}from"./Content-7d002b29.js";const n=()=>e.jsx(r,{children:e.jsx(t,{view:"update",children:e.jsx(s,{})})});export{n as default};
