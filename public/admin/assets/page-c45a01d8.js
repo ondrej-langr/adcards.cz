bf9797fda57b1770cff77e9f9226234d2639f90a
Ondřej Langr <hi@ondrejlangr.cz>
2023-11-10 12:47:55 +0100

    Update admin

---
+++ public/admin/assets/page-c45a01d8.js
@@ -0,0 +1,1 @@
+import{j as e,P as r}from"./index-2c8c6005.js";import{C as t,a as s}from"./Content-3c32898d.js";const n=()=>e.jsx(r,{children:e.jsx(t,{view:"update",children:e.jsx(s,{})})});export{n as default};
