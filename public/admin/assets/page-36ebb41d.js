2aeb5df5e63e1ab59c5e6f56f72947802572d401
Ondřej Langr <hi@ondrejlangr.cz>
2023-11-21 02:12:37 +0100

    Updates admin

---
+++ public/admin/assets/page-36ebb41d.js
@@ -0,0 +1,1 @@
+import{j as e,P as r}from"./index-d65991cf.js";import{C as t,a as s}from"./Content-db7c62a6.js";const n=()=>e.jsx(r,{children:e.jsx(t,{view:"update",children:e.jsx(s,{})})});export{n as default};
---
+++ public/admin/assets/page-7c8cbffe.js
@@ -0,0 +1,1 @@
+import{j as e,P as r}from"./index-d65991cf.js";import{C as t,a}from"./Content-db7c62a6.js";const n=()=>e.jsx(r,{children:e.jsx(t,{view:"create",children:e.jsx(a,{})})});export{n as default};
